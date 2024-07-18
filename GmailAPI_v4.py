cd1c17688ec13644d17557f853895af65b19318e
SeyeolChoi19 <78524895+SeyeolChoi19@users.noreply.github.com>
2024-07-18 17:58:10 +0900

    Update GmailAPI_v4.py

--- GmailAPI_v4.py
+++ GmailAPI_v4.py
@@ -98,27 +98,3 @@ if __name__ == "__main__":
     gapi = GmailAPI(**config_dict["GmailAPI"]["constructor"])
     gapi.gmail_settings(**config_dict["GmailAPI"]["gmail_settings"])
     gapi.send_message()
-
-
-# def gmail_authenticate():
-#     creds = None
-
-#     if os.path.exists(r"C:\Users\User\Youtube\config\token.pickle"):
-#         with open(r"C:\Users\User\Youtube\config\token.pickle", "rb") as token:
-#             creds = pickle.load(token)
-    
-#     if not creds or not creds.valid:
-#         if creds and creds.expired and creds.refresh_token:
-#             creds.refresh(Request())
-#         else:
-#             flow = InstalledAppFlow.from_client_secrets_file(r"C:\Users\User\Youtube\config\client_secret_758651627849-hf13b9bki5pk0v230r30ocqkhvctq95r.apps.googleusercontent.com.json", SCOPES)
-#             creds = flow.run_local_server(port=0)
-    
-#         with open(r"C:\Users\User\Youtube\config\token.pickle", "wb") as token:
-#             pickle.dump(creds, token)
-
-#     return build('gmail', 'v1', credentials=creds)
-
-# SCOPES = ["https://mail.google.com/"]
-
-# service_object = gmail_authenticate()
